e4ea7172c87ab657b53ed223c33d687a7d63b789
Antti Hakkarainen <a.hakkarainen@iki.fi>
2023-11-18 12:26:49 +0200

    1.2 part component

---
+++ 01/kurssitiedot/src/Part.jsx
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types'
+
+export const Part = ({ part, exercises }) => {
+    return (
+        <p>{part} {exercises}</p>
+    )
+}
+
+Part.propTypes = {
+    part: PropTypes.string.isRequired,
+    exercises: PropTypes.number.isRequired,
+}
